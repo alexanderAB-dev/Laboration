25612f3197d5361428c34b98c41b818500d5baae
alexander bartha <aabartha@gmail.com>
2021-02-05 19:58:03 +0100

    1.5

--- Laboration 1 -Alexander Bartha/Program.cs
+++ Laboration 1 -Alexander Bartha/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
+
 namespace Laboration_1__Alexander_Bartha
 {
     class Program
